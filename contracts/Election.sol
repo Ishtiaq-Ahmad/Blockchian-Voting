6aa39c210ff7e44b37c8371e8fcf81ae6d5a8346
Ishtiaq-Ahmad <67997286+Ishtiaq-Ahmad@users.noreply.github.com>
2020-07-08 19:00:02 +0500

    My first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,57 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+    //store unix time
+    uint256 public sec = block.timestamp;
+
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("Imram Khan");//candidate 1
+        addCandidate("Nawaz Shreef");//candidate 2
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+
+        //valid voting time
+        require(block.timestamp < (sec + 120));//end votting time after 120sec of deployment
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+}
